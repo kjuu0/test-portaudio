457199b93484e798c59749f3407dd7cdeeeb90b0
Brian Qiu <brianlqiu5@gmail.com>
2021-12-20 15:03:31 -0800

    init

---
+++ build/CMakeFiles/paex_record.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paex_record.dir/paex_record.c.o"
+  "CMakeFiles/paex_record.dir/paex_record.c.o.d"
+  "bin/paex_record.exe"
+  "bin/paex_record.pdb"
+  "libpaex_record.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/paex_record.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
